f05a4aba9ddfecd94747ed6fa135b303cb87a10e
Ivan Rodrigo <ivan.rodrigo.mayta@gmail.com>
2024-10-25 10:03:38 -0500

    Refactorizar archivos de configuración y eliminar llamada innecesaria a fetchLatestInvoices

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,8 +1,8 @@
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices"
-import { fetchLatestInvoices } from "@/app/lib/data"
+// import { fetchLatestInvoices } from "@/app/lib/data"
 async function Invoices() {
-    const latestInvoices = await fetchLatestInvoices()
-    return <LatestInvoices latestInvoices={latestInvoices} />
+    //const latestInvoices = await fetchLatestInvoices()
+    return <LatestInvoices />
 }
 
 export default Invoices
