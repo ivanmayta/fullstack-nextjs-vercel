d96f7dbc0e28aa6605873368c53b4dee4c846fd7
Ivan Rodrigo <ivan.rodrigo.mayta@gmail.com>
2024-10-25 09:46:57 -0500

    Agregar páginas de clientes e facturas
    Se agregaron las páginas de clientes e facturas al dashboard. También se agregó el diseño de la página y la estructura de diseño del dashboard.
    
    Refactorizar archivos de configuración
    
    Se refactorizaron los archivos de configuración para mejorar la organización y legibilidad del código.
    
    Initial commit from Create Next App
    
    Se realizó el commit inicial del proyecto utilizando Create Next App.

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+function Customers() {
+    return <h1>Customers page</h1>
+}
+
+export default Customers
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices"
+import { fetchLatestInvoices } from "@/app/lib/data"
+async function Invoices() {
+    const latestInvoices = await fetchLatestInvoices()
+    return <LatestInvoices latestInvoices={latestInvoices} />
+}
+
+export default Invoices
